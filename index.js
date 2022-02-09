69fc13e055c2b767850b022841e09e32490c75ed
GarrettHaines <ghaines1@emich.edu>
2022-02-08 23:18:56 -0500

    Fixed ad post -GH

--- index.js
+++ index.js
@@ -79,15 +79,8 @@ function printAd() {
 	
 	// Google AdSense place ad here
 	postElement.innerHTML = `
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1234567890123456" crossorigin="anonymous"></script>
-	<!-- Homepage Leaderboard -->
-	<ins class="adsbygoogle"
-	style="display:inline-block;width:600px;"
-	data-ad-client="ca-pub-1234567890123456"
-	data-ad-slot="1234567890"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5871217598891203"
+     crossorigin="anonymous"></script>
 	`;
 	
 	container.appendChild(postElement);
