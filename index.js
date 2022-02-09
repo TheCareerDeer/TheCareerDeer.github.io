8e3426ebd370310fa4dfdf429e9270b3c191000f
GarrettHaines <ghaines1@emich.edu>
2022-02-08 23:12:44 -0500

    Fixed ad post -GH

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function printAd() {
 	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1234567890123456" crossorigin="anonymous"></script>
 	<!-- Homepage Leaderboard -->
 	<ins class="adsbygoogle"
-	style="display:inline-block;width:728px;height:90px"
+	style="display:inline-block;width:600px;"
 	data-ad-client="ca-pub-1234567890123456"
 	data-ad-slot="1234567890"></ins>
 	<script>
