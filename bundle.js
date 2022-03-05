02f45214e01681082e0d81858bb168d109c6c718
GarrettHaines <ghaines1@emich.edu>
2022-03-05 17:45:33 -0500

    Implementing Firebase -GH

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-/*
+/* !
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
  * It uses "eval()" calls to create a separate source file in the browser devtools.
