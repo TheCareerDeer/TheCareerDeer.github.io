486c10f11e74b4bf8d888d5afa343548f1d197b4
hcook17 <hcook6@emich.edu>
2022-03-05 17:09:24 -0500

    Installing Firebase -GH

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWnA7vja3VhtgZbt3tnCzdKcdWl3zJA3k",
+  authDomain: "thecareerdeer.firebaseapp.com",
+  projectId: "thecareerdeer",
+  storageBucket: "thecareerdeer.appspot.com",
+  messagingSenderId: "778028835481",
+  appId: "1:778028835481:web:07af86c8b20b619c040e08"
+};
+
+initializeApp(firebaseConfig)
