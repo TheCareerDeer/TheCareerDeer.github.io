b9c0194249cc526d3539d78c8062224708636b4c
GarrettHaines <ghaines1@emich.edu>
2022-03-08 15:35:12 -0500

    Finishing Firebase -GH

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ onAuthStateChanged(auth, (user) => {
   }
   else {
 	  console.log("You are not signed in.");
+	  checkRegister();
   }
 })
 
@@ -53,90 +54,88 @@ function checkLogin() {
 }
 
 function checkRegister() {
-	
-try {
-	const registerForm = document.querySelector('#register-form');
-	const continueButton = document.getElementById("continueButton");
-	var email = "";
-	var username ="";
-	var alphaNumeric = /^([0-9]|[a-z])+([0-9a-z]+)$/i;
-	
-	function passCreds() {
-		console.log("Passing credentials to next form...");
-		email = document.getElementById("email").value;
-		username = document.getElementById("username").value;
+	try {
+		const registerForm = document.querySelector('#register-form');
+		const continueButton = document.getElementById("continueButton");
+		var email = "";
+		var username ="";
+		var alphaNumeric = /^([0-9]|[a-z])+([0-9a-z]+)$/i;
+		
+		function passCreds() {
+			console.log("Passing credentials to next form...");
+			email = document.getElementById("email").value;
+			username = document.getElementById("username").value;
+				
+			if (email != "" &&
+				email.includes("@") &&
+				email.includes(".") &&
+				email.length > 5 &&
+				!email.includes(" ") &&
+				username != "" &&
+				username.length > 2 &&
+				username.match(alphaNumeric)) {
+					
+					console.log("Requirements met...");
+					
+					const labelEmail = document.getElementById("label-email");
+					const labelUsername = document.getElementById("label-username");
+					const captionEmail = document.getElementById("caption-email");
+					const captionUsername = document.getElementById("caption-username");
+					const box1 = document.getElementById("box1");
+					const box2 = document.getElementById("box2");
+					const button1 = document.getElementById("button1");
+					
+					labelEmail.innerHTML = "<b>Password</b>";
+					labelUsername.innerHTML = "<b>Confirm Password</b>";
+					captionEmail.innerHTML = "Please enter a password that includes a number or symbol with at least eight characters.";
+					captionUsername.innerHTML = "Please retype your password to confirm it is correct.";
+					box1.innerHTML = `<input type="password" placeholder="Example: Andr0meda" id="password" name="password" required>`;
+					box2.innerHTML = `<input type="password" placeholder="Example: Andromed@" id="confirmPassword" name="confirmPassword" required>`;
+					button1.innerHTML = `<button class="button-login" style="width: 100%; height: 55px;" type="submit">COMPLETE REGISTRATION</button>`;
+					console.log("Credentials passed...");
+			}
+			else {
+				console.log("Your credentials do not meet the necessary standards required to create an account.");
+				email = "";
+				username = "";
+			}
+		}
 			
-		if (email != "" &&
-			email.includes("@") &&
-			email.includes(".") &&
-			email.length > 5 &&
-			!email.includes(" ") &&
-			username != "" &&
-			username.length > 2 &&
-			username.match(alphaNumeric)) {
-				
-				console.log("Requirements met...");
-				
-				const labelEmail = document.getElementById("label-email");
-				const labelUsername = document.getElementById("label-username");
-				const captionEmail = document.getElementById("caption-email");
-				const captionUsername = document.getElementById("caption-username");
-				const box1 = document.getElementById("box1");
-				const box2 = document.getElementById("box2");
-				const button1 = document.getElementById("button1");
-				
-				labelEmail.innerHTML = "<b>Password</b>";
-				labelUsername.innerHTML = "<b>Confirm Password</b>";
-				captionEmail.innerHTML = "Please enter a password that includes a number or symbol with at least eight characters.";
-				captionUsername.innerHTML = "Please retype your password to confirm it is correct.";
-				box1.innerHTML = `<input type="password" placeholder="Example: Andr0meda" id="password" name="password" required>`;
-				box2.innerHTML = `<input type="password" placeholder="Example: Andromed@" id="confirmPassword" name="confirmPassword" required>`;
-				button1.innerHTML = `<button class="button-login" style="width: 100%; height: 55px;" type="submit">COMPLETE REGISTRATION</button>`;
-				console.log("Credentials passed...");
-		}
-		else {
-			console.log("Your credentials do not meet the necessary standards required to create an account.");
-			email = "";
-			username = "";
-		}
-	}
-		
-	continueButton.addEventListener('click', (e) => {
-		passCreds();
-	});
+		continueButton.addEventListener('click', (e) => {
+			passCreds();
+		});
 
-	registerForm.addEventListener('submit', (e) => {
-		// Create an account
-		e.preventDefault();
-		var password = registerForm['password'].value;
-		var confirmPassword = registerForm['confirmPassword'].value;
-		console.log("Registration form submitted.");
-		
-		if(password == confirmPassword &&
-		   password.length > 7) {
-			   
-			   console.log("Your passwords match.");
-		
-			createUserWithEmailAndPassword(auth, email, password)
-			  .then((userCredential) => {
-				// Signed in 
-				console.log("Your account was created successfully.");
-				location.href = '../';
-			  })
-			  .catch((error) => {
-				const errorCode = error.code;
-				const errorMessage = error.message;
-				// ..
-			  });
-		  
-		 }
-		 else {
-			password = "";
-			confirmPassword = "";
-		 }
-	});
-}
-catch { }
+		registerForm.addEventListener('submit', (e) => {
+			// Create an account
+			e.preventDefault();
+			var password = registerForm['password'].value;
+			var confirmPassword = registerForm['confirmPassword'].value;
+			console.log("Registration form submitted.");
+			
+			if(password == confirmPassword &&
+			   password.length > 7) {
+				   
+				   console.log("Your passwords match.");
+			
+				createUserWithEmailAndPassword(auth, email, password)
+				  .then((userCredential) => {
+					// Signed in 
+					console.log("Your account was created successfully.");
+					location.href = '../';
+				  })
+				  .catch((error) => {
+					const errorCode = error.code;
+					const errorMessage = error.message;
+					// ..
+				  });
+			  
+			 }
+			 else {
+				password = "";
+				confirmPassword = "";
+			 }
+		});
+	} catch { }
 }
 
 function updateNavBar() {
