0171d6db664295fbee205c0b1a93558118e3960f
GarrettHaines <ghaines1@emich.edu>
2022-04-12 13:21:41 -0400

    Adding companies -GH

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
 	mode: "development",
-	entry: ["./src/index.js", "./src/infinite.js", "./src/saved.js"],
+	entry: ["./src/index.js", "./src/infinite.js", "./src/saved.js", "./src/companies.js"],
 	output: {
 		path: path.resolve(__dirname, "."),
 		filename: "bundle.js"
